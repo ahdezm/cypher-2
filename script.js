56f40a021ffe33abc8de9501f4b8fbe6ac28fc15
Polikis <contacto.mail99@gmail.com>
2011-05-01 18:07:40 +0000

    Updated script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function text_1(){
+function encode(){
     var text1 = document.getElementById("text_1");
-    var last = text1.value.substring(text1.value.length-1);
-    
+    var text2 = document.getElementById("text_2");
+    var key = document.getElementById("key");
+    var letter;
+    if(text1.value.length == key.value.length){
+        text2.value = "";
+        for(var i=0;i<=text1.value.length-1;i++){    
+            letter = text1.value.slice(i,i+1).charCodeAt(0)-97;
+            letter = letter + key.value.split("")[i].charCodeAt(0)-97;
+            text2.value += letter + " ";
+        }
+    }
+    else {
+        alert("The Key must be the same length as the plain Text");
+    }
 }

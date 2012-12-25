3c5fd6fe60478232f72f6c33bf984319cbcdfd16
Polikis <contacto.mail99@gmail.com>
2012-12-25 12:01:31 -0500

    Changed variable names into something more semantic

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 var cypher = function(){
     "use strict";
-    cypher.text1 = document.getElementById("text_1");
-    cypher.text2 = document.getElementById("text_2");
+    cypher.plain = document.getElementById("plain");
+    cypher.cypher = document.getElementById("cypher");
     cypher.key = document.getElementById("key");
     cypher.length1 = document.getElementById("length_1");
     cypher.length2 = document.getElementById("length_2");
     
     cypher.encode = function(){
         var letter;
-        if(this.text1.value.length === this.key.value.length){
-            this.text2.value = "";
-            this.text1.value = this.text1.value.toLowerCase();
+        if(this.plain.value.length === this.key.value.length){
+            this.cypher.value = "";
+            this.plain.value = this.plain.value.toLowerCase();
             this.key.value = this.key.value.toLowerCase();
-            for(var i=0;i<=this.text1.value.length-1;i++){    
-                letter = this.text1.value.slice(i,i+1).charCodeAt(0)-97;
+            for(var i=0;i<=this.plain.value.length-1;i++){    
+                letter = this.plain.value.slice(i,i+1).charCodeAt(0)-97;
                 /*Check for Special Characters*/
                 var special_chars = [" ",".",",","\n","_","-",":",";","?","!"];
                 var special_chars_ref = ["-65","-51","-53","-87","-2","-52","-39","-38","-34","-64"];
                 for(var a = 0; a < special_chars.length; a++){
                     if(letter.toString().match(special_chars_ref[a])){
-                        this.text2.value += special_chars[a];
+                        this.cypher.value += special_chars[a];
                     }
                 }
                 if(letter >= 0) {
@@ -28,7 +28,7 @@ var cypher = function(){
                         letter = letter-26;
                     }
                     letter = String.fromCharCode(letter+65).toLowerCase();
-                    this.text2.value += letter;
+                    this.cypher.value += letter;
                 }
             }
         }
@@ -39,25 +39,25 @@ var cypher = function(){
     cypher.decode = function(){
         var letter;
         var key_letter;
-        this.text1.value = "";
+        this.plain.value = "";
         for(var i = 0; i < this.key.value.length; i++){    
-            letter = this.text2.value.split("")[i].charCodeAt(0)-97;
+            letter = this.cypher.value.split("")[i].charCodeAt(0)-97;
             key_letter = this.key.value.slice(i,i+1).charCodeAt(0)-97;
             if(letter-key_letter < 0){
                 letter = letter + 26;
             }
             letter = letter-key_letter;
-            var character = this.text2.value.split("")[i];
+            var character = this.cypher.value.split("")[i];
             if(Array.prototype.filter.call([" ",".",",","\n","_","-",":",";","?","!"],function(self){return (self == character)}).length > 0){
-                this.text1.value += character;
+                this.plain.value += character;
             } else {
-                this.text1.value += String.fromCharCode(letter+65).toLowerCase();
+                this.plain.value += String.fromCharCode(letter+65).toLowerCase();
             }
         }
     };
     cypher.random_key = function(){
-        var string = this.text1.value;
-        if(this.text1.value.length > 0){
+        var string = this.plain.value;
+        if(this.plain.value.length > 0){
             this.key.value = "";
             window.$.get("http://www.random.org/strings/?num=1&digits=off&upperalpha=off&loweralpha=on&unique=on&format=plain&rnd=new&len=" + string.length,(function(data){
                 this.key.value = data.slice(0,string.length);
@@ -65,7 +65,7 @@ var cypher = function(){
             }).bind(this));
         }
         else {
-            alert("No plaintext avalible");    
+            alert("No plaintext avalible");
         }
     };
     
@@ -79,8 +79,8 @@ var cypher = function(){
         cypher.random_key();
     },false);
     
-    cypher.text1.addEventListener("keyup",function(){
-        cypher.length1.innerHTML = cypher.text1.value.length;
+    cypher.plain.addEventListener("keyup",function(){
+        cypher.length1.innerHTML = cypher.plain.value.length;
     },false);
     cypher.key.addEventListener("keyup",function(){
         cypher.length2.innerHTML = cypher.key.value.length;
